6617d9f2edd643be9cb3cc527a6a75c01f76b34e
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-04-07 12:16:27 +0530

    updated project
    added complete project

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // Add this line
+}
+
+android {
+    namespace = "com.example.flutter_application_1"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.flutter_application_1"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    // Add Firebase dependencies
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0")) // Use BOM for version management
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth") // For firebase_auth
+    implementation("com.google.firebase:firebase-firestore") // For cloud_firestore
+}
