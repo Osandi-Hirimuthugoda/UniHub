f617a73b6c3fdae09a48c42c484edd784022af8a
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-04-07 11:21:21 +0530

    updated backend
    updated the backend of chats and authcontroller

--- lib/controllers/authcontroller.dart
+++ lib/controllers/authcontroller.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/home.dart';
 import 'package:flutter_application_1/views/homepage.dart';
 import 'package:flutter_application_1/views/login.dart';
 import 'package:get/get.dart';
@@ -13,10 +14,16 @@ class AuthController extends GetxController {
   var passwordController = TextEditingController();
   UserCredential? userCredential;
 
+  @override
+  void onInit() {
+    super.onInit();
+    isUserAlreadyLoggedIn();
+  }
+
   isUserAlreadyLoggedIn() async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) async {
       if (user != null) {
-        Get.offAll(() => const HomeScreen());
+        Get.offAll(() => const Home());
       } else {
         Get.offAll(() => const LoginPage());
       }
@@ -24,20 +31,76 @@ class AuthController extends GetxController {
   }
 
   loginUser() async {
-    userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+    try {
+      userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      Get.offAll(() => const HomeScreen());
+    } catch (e) {
+      Get.snackbar("Login Failed", e.toString(),
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+    }
   }
 
   signupUser() async {
-    userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text);
-    await storeUserData(userCredential!.user!.uid, nameController.text, emailController.text, mobileController.text, usernameController.text);
+    try {
+      userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      User? user = userCredential!.user;
+      if (user != null) {
+        await user.updateDisplayName(usernameController.text);
+        await user.reload();
+        user = FirebaseAuth.instance.currentUser;
+        await storeUserData(
+          user!.uid,
+          nameController.text,
+          emailController.text,
+          mobileController.text,
+          usernameController.text,
+        );
+        Get.offAll(() => const HomeScreen());
+      }
+    } catch (e) {
+      Get.snackbar("Signup Failed", e.toString(),
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+      rethrow;
+    }
   }
 
   storeUserData(String uid, String fullname, String email, String mobile, String username) async {
     var store = FirebaseFirestore.instance.collection('users').doc(uid);
-    await store.set({'fullname': fullname, 'email': email, 'mobile': mobile, 'username': username});
+    await store.set({
+      'uid': uid,
+      'fullname': fullname,
+      'email': email,
+      'mobile': mobile,
+      'username': username,
+      // No default profilePic; user sets it via EditProfilePage
+      'createdAt': FieldValue.serverTimestamp(),
+    });
   }
 
   signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+}
+
+class ResetPassword {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String?> resetPassword(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      return "Password reset email sent!";
+    } catch (e) {
+      return e.toString();
+    }
   }
 }
